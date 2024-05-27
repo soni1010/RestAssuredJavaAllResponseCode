d79c2cdcdd7fe87729cb1bd8df5c800cadaf9db2
harender <hsoni2010@gmail.com>
2024-05-27 23:20:44 +0530

    Get Request Update

--- src/test/java/org/harender/testCases/GetRequests.java
+++ src/test/java/org/harender/testCases/GetRequests.java
@@ -2,33 +2,29 @@ package org.harender.testCases;
 
 
 import io.restassured.response.Response;
-import org.harender.utils.Log;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
 
 public class GetRequests {
 
-    private static final String URL = "https://reqres.in/api/users/";
+    private static final String URL = "https://reqres.in/api/";
 
     @Test(priority = 1)
     public void getRequestGetAllUsers(){
-        String allUsersBody=given().when().get(URL).getBody().asPrettyString();
+        System.out.println("\n===========================> Method - 'getRequestGetAllUsers' output below :");
+        String allUsersBody=given().when().get(URL+"users/").getBody().asPrettyString();
         System.out.println(allUsersBody);
+        System.out.println("|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|");
+
     }
 
     @Test(priority = 2)
     public void getRequestGetParticularUserInformation(){
+        System.out.println("\n===========================> Method - 'getRequestGetParticularUserInformation' output below :");
+
         int userID=2;
-        Response response=given().when().get(URL+userID);
+        Response response=given().when().get(URL+"users/"+userID);
         System.out.println(
                 "Status Code - "+response.getStatusCode()+
                 "\nSimple Response.getTime -"+response.getTime()+
@@ -45,56 +41,62 @@ public class GetRequests {
                 "\n\nresponse.getDetailedCookies() - "+response.getDetailedCookies()+
                 "\n\nresponse.getStatusLine() - "+response.getStatusLine()
         );
+        System.out.println("|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|");
 
     }
 
-//    @DataProvider(name = "getUserData")
-//    public Iterator<Object[]> getUsers() {
-//        final List<Object[]> getData = new ArrayList<>();
-//        getData.add(new Object[]{2});
-//        return getData.iterator();
-//    }
+    @Test(priority = 3)
+    public void tryGetRequestGetParticularUserInformationWhichIsNotAvailable(){
+        System.out.println("\n===========================> Method - 'tryGetRequestGetParticularUserInformationWhichIsNotAvailable' output below :");
+        int notAvailableRecordUserID=23;
+        Response response=given().when().get(URL+"users/"+notAvailableRecordUserID);
+        System.out.println(
+                "Status Code - "+response.getStatusCode()+
+                "\nSimple Response.getTime -"+response.getTime()+
+                "\n\nResponse.getTime(TimeUnit.MILLISECONDS(Default)) - "+response.getTimeIn(TimeUnit.MILLISECONDS)+
+                "\nResponse.getTime(TimeUnit.SECONDS) - "+response.getTimeIn(TimeUnit.SECONDS)+
+                "\nWe can take in Minutes,Days,Hours,Seconds,Nanoseconds,Microseconds,MilliSeconds also"+
 
-//    @Test(dataProvider = "getUserData")
-//    public void getRequestTest(final int userId) {
-//        given().when()
-//                .get(URL + userId)
-//                .then()
-//                .statusCode(200)
-//                .and()
-//                .assertThat()
-//                .body("data.id", equalTo(userId));
-//
-//        System.out.println(given().when().get(URL+userId).body().prettyPrint());
-//        final int statusCode = given().when()
-//                .get(URL + userId)
-//                .statusCode();
-//        Log.info(statusCode);
-//
-//        final String responseBody = given().when()
-//                .get(URL + userId)
-//                .getBody()
-//                .asString();
-//        Log.info(responseBody);
-//    }
-//
-//    @Test(dataProvider = "getUserData")
-//    public void getRequestTestWithQueryParam(final int userPage) {
-//        given().when()
-//                .queryParam("page", userPage)
-//                .get(URL)
-//                .then()
-//                .statusCode(200)
-//                .and()
-//                .assertThat()
-//                .body("page", equalTo(userPage));
-//
-//        final String responseBody = given().when()
-//                .queryParam("page", userPage)
-//                .get(URL)
-//                .getBody()
-//                .asString();
-//        Log.info(responseBody);
-//    }
+                "\n\nresponse.getContentType()) - "+response.getContentType()+
+
+                "\n\nresponse.getHeaders() - "+response.getHeaders()+
+                "\n\nresponse.getHeader('Report-To') - "+response.getHeader("Report-To")+
+                "\n\nresponse.getHeader('Reporting-Endpoints') - "+response.getHeader("Reporting-Endpoints")+
+
+                "\n\nresponse.getSessionId() - "+response.getSessionId()+
+                "\n\nresponse.getCookies() - "+response.getCookies()+
+                "\n\nresponse.getDetailedCookies() - "+response.getDetailedCookies()+
+                "\n\nresponse.getStatusLine() - "+response.getStatusLine());
+        System.out.println("|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|");
+
+    }
+
+    @Test(priority = 4)
+    public void tryGetRequestListOfResources(){
+        System.out.println("\n===========================> Method - 'tryGetRequestListOfResources' output below :");
+        String endPointForListOdfResources="unknown";
+        Response response=given().when().get(URL+endPointForListOdfResources);
+        System.out.println("Status Code - "+response.getStatusCode()+
+                        "\nSimple Response.getTime -"+response.getTime()+
+                        "\n\nResponse.getTime(TimeUnit.MILLISECONDS(Default)) - "+response.getTimeIn(TimeUnit.MILLISECONDS)+
+                        "\nResponse.getTime(TimeUnit.SECONDS) - "+response.getTimeIn(TimeUnit.SECONDS)+
+                        "\nWe can take in Minutes,Days,Hours,Seconds,Nanoseconds,Microseconds,MilliSeconds also"+
+
+                        "\n\nresponse.getContentType() - "+response.getContentType()+
+
+                        "\n\nresponse.getHeaders() - "+response.getHeaders()+
+                        "\n\nresponse.getHeader('Report-To') - "+response.getHeader("Report-To")+
+                        "\n\nresponse.getHeader('Reporting-Endpoints') - "+response.getHeader("Reporting-Endpoints")+
+
+                        "\n\nresponse.getSessionId() - "+response.getSessionId()+
+                        "\n\nresponse.getCookies() - "+response.getCookies()+
+                        "\n\nresponse.getDetailedCookies() - "+response.getDetailedCookies()+
+                        "\n\nresponse.getStatusLine() - "+response.getStatusLine()+
+                "\n\n Response "+ response.getBody().asPrettyString()
+                );
+        System.out.println("|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|");
+
+    }
+
 
 }
